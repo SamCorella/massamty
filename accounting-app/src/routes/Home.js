854d53b14f98e2450acab7b2bc7f20fb5e37ddf1
Sam Corella <samcorella77@gmail.com>
2023-10-12 22:10:36 -0400

    Add data grid to Home for Chart of Accounts

--- accounting-app/src/routes/Home.js
+++ accounting-app/src/routes/Home.js
@@ -1,7 +1,24 @@
+import { DataGrid } from "@mui/x-data-grid";
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
 
 function Home() {
-  return <ResponsiveAppBar />;
+  const columns = [
+    { field: "id", headerName: "User ID", flex: 1 },
+    { field: "AccountName", headerName: "Account Name", flex: 1 },
+    { field: "AccountNumber", headerName: "Account Number", flex: 1 },
+    { field: "AccountDescription", headerName: "Account Description", flex: 1 },
+    { field: "Balance", headerName: "Balance", flex: 1 },
+  ];
+
+  const rows = [];
+
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <ResponsiveAppBar />
+      <h2>Chart of Accounts</h2>
+      <DataGrid rows={rows} columns={columns} />
+    </div>
+  );
 }
 
 export default Home;
