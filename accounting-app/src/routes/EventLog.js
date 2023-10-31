c468cbede02f068d2931ad467a888c1caa48b728
Mason Farmer <mfarme49@students.kennesaw.edu>
2023-10-31 12:09:35 -0400

    Event Log

---
+++ accounting-app/src/routes/EventLog.js
@@ -0,0 +1,17 @@
+import { DataGrid } from "@mui/x-data-grid";
+
+function EventLog() {
+    const columns = [
+        { field: "id", headerName: "User ID", flex: 1 },
+        { field: "before", headerName: "Before", flex: 1 },
+        { field: "after", headerName: "After", flex: 1 },
+        { field: "date", headerName: "Date", flex: 1 },
+    ];
+    return (
+        <div style={{ height: 400, width: "100%" }}>
+            <DataGrid
+            />
+        </div>
+    );
+};
+export default EventLog;
